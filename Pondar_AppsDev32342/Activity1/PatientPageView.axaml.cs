c27555afa1bc708202ec06db4b0195e0679e6742
Theo <143622653+Speender@users.noreply.github.com>
2025-04-29 15:25:28 +0800

    feat: unfinished patient layout pay naa

---
+++ Pondar_AppsDev32342/Activity1/PatientPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HospitalApp.Views{
+    public partial class PatientPageView : UserControl
+    {
+        public PatientPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
